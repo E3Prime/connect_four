12211e398a229a1215ae79b0a600ea0d78aa8a1c
E3Prime <primesoftware3@gmail.com>
2026-02-11 22:12:55 -0700

    still working on design and worked on functionality a bit

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ const performMenuAction = async (e: PointerEvent) => {
     await sleep(1);
     menuOverlayElem.classList.add("overlay-transition");
     await sleep(700);
-    difficultyOptionsElem.classList.add("difficulty-options-reveal");
+    difficultyOptionsElem.classList.add("options-reveal");
     return;
   }
 
@@ -31,10 +31,19 @@ const performMenuAction = async (e: PointerEvent) => {
   gameRulesElem.classList.remove("fly-from-right");
 };
 
+const gameMenuOptions = async (e: PointerEvent) => {
+  if (!(e.target instanceof HTMLButtonElement)) return;
+  gameMenuOverlay.removeAttribute("hidden");
+  await sleep(1);
+  gameMenuOverlay.classList.add("overlay-transition");
+  await sleep(700);
+  gameMenuOptionsElem.classList.add("options-reveal");
+};
+
 const difficultyDecision = async (e: PointerEvent) => {
   if (!(e.target instanceof HTMLButtonElement)) return;
   const chosenDifficulty = e.target.id;
-  difficultyOptionsElem.classList.remove("difficulty-options-reveal");
+  difficultyOptionsElem.classList.remove("options-reveal");
   await sleep(500);
   menuOverlayElem.classList.remove("overlay-transition");
   menuElem.classList.add("fly-off-left");
@@ -51,10 +60,14 @@ const menuOverlayElem = menuElem.nextElementSibling as HTMLElement;
 const difficultyOptionsElem = menuOverlayElem.firstElementChild as HTMLElement;
 const gameRulesElem = document.getElementById("gameRules") as HTMLElement;
 const gameElem = document.getElementById("game") as HTMLElement;
+const openGameMenuBtn = gameElem.querySelector("#openGameMenu") as HTMLButtonElement;
+const gameMenuOverlay = gameElem.querySelector("#gameMenuOverlay") as HTMLElement;
+const gameMenuOptionsElem = gameMenuOverlay.firstElementChild as HTMLElement;
 const boardElem = gameElem.querySelector("#board") as HTMLElement;
 
 menuElem.addEventListener("click", performMenuAction);
 gameRulesElem.addEventListener("click", returnToMainMenu);
+openGameMenuBtn.addEventListener("click", gameMenuOptions);
 difficultyOptionsElem.addEventListener("click", difficultyDecision);
 
 async function startGame(chosenDifficulty: string) {
@@ -66,6 +79,16 @@ async function startGame(chosenDifficulty: string) {
 
 function renderBoard() {
   boardElem.innerHTML = "";
+  for (let i = 0; i < 42; i++) {
+    const slot = document.createElement("div");
+    slot.className = "relative flex items-center justify-center h-full w-full";
+    const col = i % 7;
+    const row = Math.floor(i / 7);
+    slot.dataset.col = col.toString();
+    slot.dataset.row = row.toString();
+
+    boardElem.appendChild(slot);
+  }
 }
 
 startGame("hard");
